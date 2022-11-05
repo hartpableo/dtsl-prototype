6a3efa32fd6bee39ad52e48ad22cf61a39cfecea
Hart Lorenzo <pableoh@gmail.com>
2022-11-05 16:56:41 +0800

    news section

--- js/main.js
+++ js/main.js
@@ -1,10 +1,43 @@
 $(document).ready(function() {
     // update year in copyright section
     updateYear();
+
+    // slick slider
+    activateSlick();
 });
 
 // declare variables
 var date = $('.c-date');
+
+// slick slider
+function activateSlick() {
+    $('.articles-slider').slick(
+        {
+            prevArrow : $('.slider-nav__button.prev-button'),
+            nextArrow : $('.slider-nav__button.next-button'),
+            mobileFirst: true,
+            infinite: true,
+            slidesToScroll: 1,
+            responsive: [{
+                breakpoint: 0,
+                settings: {
+                    slidesToShow: 1,
+                    centerMode:true,
+                },
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+                    centerMode:false,
+                },
+                // breakpoint: 992,
+                // settings: {
+                //     slidesToShow: 2,
+                //     centerMode:false,
+                // },
+            }
+        ],
+      });
+}
 
 // update year in copyright section
 function updateYear() {
